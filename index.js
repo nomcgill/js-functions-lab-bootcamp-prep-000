1660a37e921b597e94af6db021585348e8255639
Nolan McGill <nomcgill@gmail.com>
2018-08-30 01:39:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ function happyHolidays() {
 return "Happy holidays!"
 }
 
-function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!'
-}
+
 
 function happyHolidayTo(holiday, name) {
 return 'Happy ${holiday}, ${name!}'
